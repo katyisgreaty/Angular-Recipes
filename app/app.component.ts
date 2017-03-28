d8dd5b078d046211a57be8c75100470345fed9e5
Katy Daviscourt and Keely Glenn <katy+keely@>
2017-03-28 10:41:53 -0700

    add property binding and event binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
     <h1>RECIPEEEESSSSSSSS</h1>
     <h3>{{currentFoodMood}} - {{month}}/{{day}}/{{year}}</h3>
     <ul>
-      <li *ngFor="let currentRecipe of recipes">{{currentRecipe.title}}, {{currentRecipe.ingredients}}, {{currentRecipe.directions}}</li>
+      <li [class]="tastinessColor(currentRecipe)" (click)="isBaked(currentRecipe)" *ngFor="let currentRecipe of recipes" >{{currentRecipe.title}} <button (click)='editRecipe()'>Edit</button></li>
     </ul>
   </div>
   `
@@ -20,13 +20,35 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   recipes: Recipe[] = [
-    new Recipe('Chocolate Cake', 'cocoa powder, flour, water, eggs', 'Put it all together and youre good to go'),
-    new Recipe('Mac and Cheese', 'macaroni and cheese', 'Put it all together and youre good to go'),
-    new Recipe('Peanut Butter Bars', 'Peanut butter, powdered sugar, butter, crushed graham crackers, melted chocolate chips', 'melt butter, mix together all ingredients except chocolate, put into brownie pan, drizzle chocolate to cover top, put in fridge for 2 hours.')
+    new Recipe('Chocolate Cake', 'cocoa powder, flour, water, eggs', 'Put it all together and youre good to go', 1),
+    new Recipe('Mac and Cheese', 'macaroni and cheese', 'Put it all together and youre good to go', 2),
+    new Recipe('Peanut Butter Bars', 'Peanut butter, powdered sugar, butter, crushed graham crackers, melted chocolate chips', 'melt butter, mix together all ingredients except chocolate, put into brownie pan, drizzle chocolate to cover top, put in fridge for 2 hours.', 3)
   ];
+
+  editRecipe() {
+    alert("You just requested to edit this recipe!");
+  }
+
+  isBaked(clickedRecipe: Recipe) {
+    if(clickedRecipe.baked === true) {
+      alert("This recipe is baked!");
+    } else {
+      alert("This recipe is not baked!");
+    }
+  }
+
+    tastinessColor(currentRecipe){
+    if (currentRecipe.tastiness === 1){
+      return "bg-danger";
+    } else if (currentRecipe.tastiness === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
 }
 
 export class Recipe {
   public baked: boolean = false;
-  constructor(public title: string, public ingredients: string, public directions: string) { }
+  constructor(public title: string, public ingredients: string, public directions: string, public tastiness: number) { }
 }
